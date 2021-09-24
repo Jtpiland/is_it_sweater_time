a3cc9d2511cfdca2ea9d99e8917f36339156c23c
Jacob Piland <piland.jacob@gmail.com>
2021-09-24 00:50:50 -0500

    Add weather service

---
+++ app/services/weather_service.rb
@@ -0,0 +1,22 @@
+class WeatherService
+  class << self
+    def get_current_weather(lat, lon)
+
+      result = conn.get("/data/2.5/weather") do |req|
+        req.params[:lat] = "#{lat}"
+        req.params[:lon] = "#{lon}"
+      end
+      parse_json(result)
+    end
+
+    def conn
+      Faraday.new(url: "https://api.openweathermap.org") do |req|
+        req.params[:appid] = ENV['weather_key']
+      end
+    end
+
+    def parse_json(result)
+      formatted_result = JSON.parse(result.body, synmbolize_names: true)
+    end
+  end
+end
