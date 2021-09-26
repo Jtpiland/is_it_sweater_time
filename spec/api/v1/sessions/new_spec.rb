869e55bece5c570852081a8b05e5e653333af9e5
Jacob Piland <piland.jacob@gmail.com>
2021-09-26 11:50:55 -0500

    Add error testing to new sessions spec

--- spec/api/v1/sessions/new_spec.rb
+++ spec/api/v1/sessions/new_spec.rb
@@ -7,8 +7,13 @@ RSpec.describe 'Sessions' do
     @user.api_key = User.set_api_key
     @user.save
   end
+  
   describe 'POST /api/v1/users' do
     let(:valid_attributes) { { 'email': 'funbucket@gmail.com', 'password': 'pw'} }
+
+    let(:invalid_account) { { 'email': 'funbucket@gmai.com', 'password': 'pw'} }
+
+    let(:invalid_password) { { 'email': 'funbucket@gmail.com', 'password': 'p'} }
 
     it 'can create a session with valid credentials', :vcr do
       headers = { "Content-Type" => "application/json",
@@ -32,6 +37,38 @@ RSpec.describe 'Sessions' do
       expect(x[:data][:attributes][:email]).to be_a(String)
       expect(x[:data][:attributes]).to have_key(:api_key)
       expect(x[:data][:attributes][:api_key]).to be_a(String)
+    end
+
+    it 'can send an error message if an account has not been created', :vcr do
+      headers = { "Content-Type" => "application/json",
+      "Accept" => "application/json" }
+
+      post "/api/v1/sessions", headers: headers, params: JSON.generate(invalid_account)
+
+      x = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(404)
+      expect(x).to be_a(Hash)
+      expect(x).to have_key(:data)
+      expect(x[:data]).to be_a(Hash)
+      expect(x[:data]).to have_key(:error)
+      expect(x[:data][:error]).to eq("Please create an account.")
+    end
+
+    it 'can send an error message if given invalid credentials', :vcr do
+      headers = { "Content-Type" => "application/json",
+      "Accept" => "application/json" }
+
+      post "/api/v1/sessions", headers: headers, params: JSON.generate(invalid_password)
+
+      x = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(404)
+      expect(x).to be_a(Hash)
+      expect(x).to have_key(:data)
+      expect(x[:data]).to be_a(Hash)
+      expect(x[:data]).to have_key(:error)
+      expect(x[:data][:error]).to eq("Invalid Credentials.")
     end
   end
 end
