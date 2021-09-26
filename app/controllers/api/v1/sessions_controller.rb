9a704040930b67a6eac894832c31e41c72aadea0
Jacob Piland <piland.jacob@gmail.com>
2021-09-26 11:51:14 -0500

    Refactor sessions controller to add error handling

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,23 +4,18 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user.nil?
-      # flash[:error] = 'Email can not be blank.'
-      # redirect_to root_path
-      render json: :bad_request
+      render json: {data:{error:'Please create an account.'}}, :status => :not_found
     elsif user.authenticate(params[:password])
       session[:user_id] = user.id
 
       render json: SessionSerializer.session(user)
     else
-      flash[:error] = 'Sorry, your credentials are bad.'
-      redirect_to root_path
-      #render json?
+      render json: {data:{error:'Invalid Credentials.'}}, :status => :not_found
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
-  # end
+    render json: {data:{}}, :status => 200
   end
 end
