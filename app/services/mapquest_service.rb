8984e3ff219e41d0dff641adf151620037ea4050
Jacob Piland <piland.jacob@gmail.com>
2021-09-27 17:50:50 -0500

    Add directions method to mapquest service

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -1,6 +1,15 @@
 class MapquestService
 
   class << self
+
+    def get_directions(from, to)
+      result = conn.get("/directions/v2/route") do |req|
+        req.params['from'] = "#{from}"
+        req.params['to'] = "#{to}"
+      end
+      parse(result)
+    end
+
     def get_location(location)
       result = conn.get("/geocoding/v1/address") do |req|
         req.params['location'] = "#{location}"
@@ -16,6 +25,10 @@ class MapquestService
 
     def parse_json(result)
       formatted_result = JSON.parse(result.body, symbolize_names: true)[:results][0]
+    end
+
+    def parse(result)
+      formatted_result = JSON.parse(result.body, symbolize_names: true)
     end
   end
 end
