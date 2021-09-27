b54595918c6a3d92a5af4d03b74c38121ad95ecc
Jacob Piland <piland.jacob@gmail.com>
2021-09-27 12:17:39 -0500

    Add book search facade

---
+++ app/facades/book_search_facade.rb
@@ -0,0 +1,16 @@
+class BookSearchFacade
+  def self.fetch_books(location)
+    result = MapquestService.get_location(location)
+    lat = result[:locations][0][:latLng][:lat]
+    lon = result[:locations][0][:latLng][:lng]
+
+    weather = WeatherService.get_weather("#{lat}", "#{lon}")
+
+    current_weather = CurrentWeather.new(weather[:current])
+    books = BookService.get_books(location)
+
+    book_search = BookSearch.new(current_weather, books)
+
+    book_search_data = BookSearchSerializer.book_search(book_search, location)
+  end
+end
