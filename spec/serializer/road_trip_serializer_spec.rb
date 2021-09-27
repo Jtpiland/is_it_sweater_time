d64619407e20d5d52b927db9aa0f6d1d09bc52de
Jacob Piland <piland.jacob@gmail.com>
2021-09-27 17:52:01 -0500

    Add road trip serializer spec

---
+++ spec/serializer/road_trip_serializer_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'RoadTripSerializer' do
+  it 'can return serialize weather and direction info' do
+    from = 'Denver, CO'
+    to = 'Estes Park, CO'
+
+    weather = {
+                dt: 1632690000,
+                temp: 87.12,
+                weather: [{
+                description: "few clouds",
+                icon: "02d"
+                        }]
+              }
+
+    hourly_weather = HourlyWeather.new(weather)
+
+    d = {
+      formattedTime: '01:22:49'
+    }
+
+    directions = Directions.new(d)
+
+    road_trip = RoadTrip.new(hourly_weather, directions)
+    road_trip_data = RoadTripSerializer.road_trip(road_trip, from, to)
+
+    expect(road_trip_data).to be_a(Hash)
+    expect(road_trip_data[:data]).to have_key(:id)
+    expect(road_trip_data[:data]).to have_key(:type)
+    expect(road_trip_data[:data][:type]).to be_a(String)
+    expect(road_trip_data[:data]).to have_key(:attributes)
+    expect(road_trip_data[:data][:attributes]).to be_a(Hash)
+    expect(road_trip_data[:data][:attributes]).to have_key(:start_city)
+    expect(road_trip_data[:data][:attributes][:start_city]).to be_a(String)
+    expect(road_trip_data[:data][:attributes]).to have_key(:end_city)
+    expect(road_trip_data[:data][:attributes][:end_city]).to be_a(String)
+    expect(road_trip_data[:data][:attributes]).to have_key(:travel_time)
+    expect(road_trip_data[:data][:attributes][:travel_time]).to be_a(String)
+    expect(road_trip_data[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Numeric)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+  end
+end
