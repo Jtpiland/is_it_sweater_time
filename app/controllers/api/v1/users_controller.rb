abbf858c89a269308dc28dd67a99301102c95b7a
Jacob Piland <piland.jacob@gmail.com>
2021-09-28 13:56:49 -0500

    Refactor user create with a bang to use rescue from

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
 # skip_before_action :authorized?
 
   def create
-    user = User.create(user_params)
+    user = User.create!(user_params)
     user.api_key = User.set_api_key
     # session[:user_id] = user.id
     user.save
