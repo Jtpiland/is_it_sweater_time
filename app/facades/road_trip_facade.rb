19a9dfafb1534e14eb517893f575079a034cdff8
Jacob Piland <piland.jacob@gmail.com>
2021-09-28 11:39:26 -0500

    Refactor road trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -9,11 +9,11 @@ class RoadTripFacade
     lon = result[:locations][0][:latLng][:lng]
 
     weather = WeatherService.get_weather("#{lat}", "#{lon}")
-    hourly_weather =    weather[:hourly][0..7].map do |hourly|
+    hourly_weather =    weather[:hourly].map do |hourly|
                           HourlyWeather.new(hourly)
                         end
 
-    road_trip = RoadTrip.new(hourly_weather[0], directions)
+    road_trip = RoadTrip.new(hourly_weather[directions.travel_time.split(':').first.to_i], directions)
 
     RoadTripSerializer.road_trip(road_trip, from, to)
   end
