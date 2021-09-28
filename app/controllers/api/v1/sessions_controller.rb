b4c5129f92ea075f4c7fb122d5758e52f032639d
Jacob Piland <piland.jacob@gmail.com>
2021-09-28 14:08:52 -0500

    Remove destroy session action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
-#   skip_before_action :authorized?
-#
+
   def create
     user = User.find_by(email: params[:email])
     if user.nil?
@@ -12,10 +11,5 @@ class Api::V1::SessionsController < ApplicationController
     else
       render json: {data:{error:'Invalid Credentials.'}}, :status => :not_found
     end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    render json: {data:{}}, :status => 200
   end
 end
