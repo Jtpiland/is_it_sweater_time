9e21b51eff59e8a590d37150dffb73db8f6e3f35
Jacob Piland <piland.jacob@gmail.com>
2021-09-27 22:49:31 -0500

    Add road trip controller

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::RoadTripController < ApplicationController
+
+  def create
+    from = params['origin']
+    to = params['destination']
+
+    User.find_by(api_key: params['api_key'])
+    require "pry"; binding.pry
+    render json: RoadTripFacade.fetch_directions_and_weather(from, to)
+  end
+end
