50af09b0e4d4e8fdba95bf19ddc6f110a774775e
Jacob Piland <piland.jacob@gmail.com>
2021-09-28 14:09:00 -0500

    Remove extra space

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     background = BackgroundsFacade.fetch_image(params[:query])
 
-    render json: background, status: 200 
+    render json: background, status: 200
   end
-
 end
