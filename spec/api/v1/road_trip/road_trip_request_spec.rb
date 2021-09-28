2d87b0b40b3f1b8468b5005448d9c513315f9cf7
Jacob Piland <piland.jacob@gmail.com>
2021-09-28 11:39:08 -0500

    Refactor road trip request spec and add sad path test

--- spec/api/v1/road_trip/road_trip_request_spec.rb
+++ spec/api/v1/road_trip/road_trip_request_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Road Trip Request' do
-  let(:valid_attributes) { {'origin': "Denver,CO", 'destination': "Pueblo,CO", 'api_key': "jgn983hy48thw9begh98h4539h4"} }
+  let(:valid_attributes) { {'origin': "Denver, CO", 'destination': "Pueblo, CO", 'api_key': "jgn983hy48thw9begh98h4539h4"} }
 
-  it 'can return serialized information about a road trip', :vcr do
+  it 'can return serialized information about a road trip with valid credentials', :vcr do
     user = User.create(email: 'email@email.com', password: 'pw', password_confirmation: 'pw')
 
     user.update(api_key: "jgn983hy48thw9begh98h4539h4")
+
     headers = { "Content-Type" => "application/json",
     "Accept" => "application/json" }
     post '/api/v1/road_trip', headers: headers, params: JSON.generate(valid_attributes)
@@ -14,5 +15,28 @@ RSpec.describe 'Road Trip Request' do
     x = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+    expect(x[:data][:id]).to eq(nil)
+    expect(x[:data][:type]).to eq('roadtrip')
+    expect(x[:data][:attributes][:start_city]).to eq('Denver, CO')
+    expect(x[:data][:attributes][:end_city]).to eq('Pueblo, CO')
+    expect(x[:data][:attributes][:travel_time]).to eq('01:44:22')
+    expect(x[:data][:attributes][:weather_at_eta][:temperature]).to eq(69.76)
+    expect(x[:data][:attributes][:weather_at_eta][:conditions]).to eq('few clouds')
+  end
+
+  it 'can return error if invalid credentials are provided', :vcr do
+    user = User.create(email: 'email@email.com', password: 'pw', password_confirmation: 'pw')
+
+    user.update(api_key: "jgn983hy48thw9begh98h4539h3")
+    
+    headers = { "Content-Type" => "application/json",
+    "Accept" => "application/json" }
+    post '/api/v1/road_trip', headers: headers, params: JSON.generate(valid_attributes)
+
+    x = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+    expect(x).to eq({ :error => "Invalid Credentials" })
   end
 end
