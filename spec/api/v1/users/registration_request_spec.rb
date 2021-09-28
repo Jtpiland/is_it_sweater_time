a514e79045d42a9c781f50162e8179a91b66cd2f
Jacob Piland <piland.jacob@gmail.com>
2021-09-28 13:54:09 -0500

    Refactor spec for sad paths

--- spec/api/v1/users/registration_request_spec.rb
+++ spec/api/v1/users/registration_request_spec.rb
@@ -2,26 +2,18 @@ require 'rails_helper'
 require './lib/modules/api_key'
 
 RSpec.describe 'Registration Request' do
-  # before :each do
-  #   post "/api/v1/users", params: { email: 'email', password: 'password', password_confirmation: 'password'}
-  #
-  #   x = JSON.parse(response.body, symbolize_names: true)
-  # end
   describe 'POST /api/v1/users' do
     let(:valid_attributes) { { 'email': 'email', 'password': 'password', 'password_confirmation': 'password' } }
-    # headers = { "Content-Type" => "application/json" }
 
-    it 'can register and save a new user to the database', :vcr do
+    let(:invalid_email) { { 'email': '', 'password': 'password', 'password_confirmation': 'password' } }
+
+    let(:invalid_password) { { 'email': 'email', 'password': 'password', 'password_confirmation': 'pw' } }
+
+    it 'can register and save a new user to the database' do
       headers = { "Content-Type" => "application/json",
       "Accept" => "application/json" }
 
       expect(User.all.length).to be(0)
-
-      # post "/api/v1/users", headers: headers, params: { email: 'email', password: 'password', password_confirmation: 'password' }
-
-      # post "/api/v1/users", headers: headers, request.body => valid_attributes
-
-      # body(JSON.generate({user: valid_attributes}))
 
       post "/api/v1/users", headers: headers, params: JSON.generate(valid_attributes)
 
@@ -30,18 +22,41 @@ RSpec.describe 'Registration Request' do
       expect(response.status).to eq(201)
       expect(User.all.length).to eq(1)
       expect(x).to be_a(Hash)
-      expect(x).to have_key(:data)
-      expect(x[:data]).to be_a(Hash)
-      expect(x[:data]).to have_key(:type)
-      expect(x[:data][:type]).to be_a(String)
-      expect(x[:data]).to have_key(:id)
       expect(x[:data][:id]).to be_a(Integer)
-      expect(x[:data]).to have_key(:attributes)
-      expect(x[:data][:attributes]).to be_a(Hash)
-      expect(x[:data][:attributes]).to have_key(:email)
-      expect(x[:data][:attributes][:email]).to be_a(String)
+      expect(x[:data][:type]).to eq('user')
+      expect(x[:data][:attributes][:email]).to eq('email')
       expect(x[:data][:attributes]).to have_key(:api_key)
       expect(x[:data][:attributes][:api_key]).to be_a(String)
+    end
+
+    it 'will send an error message with invalid email' do
+      headers = { "Content-Type" => "application/json",
+      "Accept" => "application/json" }
+
+      expect(User.all.length).to be(0)
+
+      post "/api/v1/users", headers: headers, params: JSON.generate(invalid_email)
+
+      x = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(400)
+      expect(User.all.length).to eq(0)
+      expect(x[:email]).to eq(["can't be blank"])
+    end
+
+    it 'will send an error message with invalid password' do
+      headers = { "Content-Type" => "application/json",
+      "Accept" => "application/json" }
+
+      expect(User.all.length).to be(0)
+
+      post "/api/v1/users", headers: headers, params: JSON.generate(invalid_password)
+
+      x = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(400)
+      expect(User.all.length).to eq(0)
+      expect(x[:password_confirmation]).to eq(["doesn't match Password", "doesn't match Password"])
     end
   end
 end
