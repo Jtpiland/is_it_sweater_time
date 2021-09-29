c65b2f2a6ab6637b5dfa438071bc36c7cbc8310f
Jacob Piland <piland.jacob@gmail.com>
2021-09-28 19:45:59 -0500

    Remove commented out method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,17 +1,6 @@
 class ForecastFacade
   class << self
 
-  #   def fetch_weather(location)
-  #     result = MapquestService.get_location(location)
-  #     lat = result[:locations][0][:latLng][:lat]
-  #     lon = result[:locations][0][:latLng][:lng]
-  #
-  #     weather = WeatherService.get_weather("#{lat}", "#{lon}")
-  #
-  #     WeatherSerializer.weather(weather)
-  #   end
-  # end
-
     def fetch_weather(location)
       result = MapquestService.get_location(location)
       lat = result[:locations][0][:latLng][:lat]
