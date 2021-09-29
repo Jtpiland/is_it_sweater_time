466408482fc0f8417cea4012df95604b13b1aaec
Jacob Piland <piland.jacob@gmail.com>
2021-09-28 22:22:06 -0500

    Add impossible route to road trip serializer

--- spec/serializer/road_trip_serializer_spec.rb
+++ spec/serializer/road_trip_serializer_spec.rb
@@ -44,4 +44,30 @@ RSpec.describe 'RoadTripSerializer' do
     expect(road_trip_data[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
     expect(road_trip_data[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
   end
+
+  it 'can return json for impossible route if no directions are found' do
+    from = 'Denver, CO'
+    to = 'London, UK'
+
+    road_trip_data = RoadTripSerializer.impossible_road_trip(from, to)
+
+    expect(road_trip_data).to be_a(Hash)
+    expect(road_trip_data[:data]).to have_key(:id)
+    expect(road_trip_data[:data]).to have_key(:type)
+    expect(road_trip_data[:data][:type]).to be_a(String)
+    expect(road_trip_data[:data]).to have_key(:attributes)
+    expect(road_trip_data[:data][:attributes]).to be_a(Hash)
+    expect(road_trip_data[:data][:attributes]).to have_key(:start_city)
+    expect(road_trip_data[:data][:attributes][:start_city]).to be_a(String)
+    expect(road_trip_data[:data][:attributes]).to have_key(:end_city)
+    expect(road_trip_data[:data][:attributes][:end_city]).to be_a(String)
+    expect(road_trip_data[:data][:attributes]).to have_key(:travel_time)
+    expect(road_trip_data[:data][:attributes][:travel_time]).to be_a(String)
+    expect(road_trip_data[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta][:temperature]).to be(nil)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+    expect(road_trip_data[:data][:attributes][:weather_at_eta][:conditions]).to be(nil)
+  end
 end
